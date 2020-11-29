070ce025cab4df262ee32f2ea9d1bbc0fda00248
Simon Weber <simon@simonmweber.com>
2020-11-29 18:32:20 -0500

    cleanup lib

--- repominder/lib/ghapp.py
+++ repominder/lib/ghapp.py
@@ -34,7 +34,7 @@ def cache_repos(user):
     # should only be run as a result of a user logging in (ie, not from a cron or script)
     social = user.social_auth.get(provider="github-app")
 
-    # user access token
+    # this is a user access token
     gh = Github(social.extra_data["access_token"])
     repos = gh.get_user().get_repos()
     accessible_names = [
@@ -50,16 +50,9 @@ def cache_repos(user):
     user.profile.save()
 
 
-def get_app_url(app_name):
-    return "https://github.com/apps/%s" % app_name
-
-
 def get_session():
-    """Return a requests.Session for use with the apps api."""
-
     s = requests.Session()
     s.headers.update({"Accept": "application/vnd.github.v3+json"})
-
     return s
 
 
@@ -135,43 +128,3 @@ def get_installation_repos(installation):
     res.raise_for_status()
 
     return res
-    # current_names = {repo['full_name'] for repo in res.json()}
-
-    # # bulk create/update repos, then bulk create/update links for this user
-    # with transaction.atomic():
-    #     cached_names = set(Repo.objects.filter(full_name__in=current_names).values_list('full_name', flat=True))
-    #     new_names = current_names - cached_names
-    #     new_repos = [Repo(full_name=name) for name in new_names]
-    #     if new_repos:
-    #         logger.info("creating repos: %r", new_repos)
-    #         Repo.objects.bulk_create(new_repos)
-
-    #     new_ids = Repo.objects.filter(full_name__in=new_names).values_list('id', flat=True)
-    #     new_links = [UserRepo(repo_id=id, user=user) for id in new_ids]
-    #     if new_links:
-    #         logger.info("creating links: %r", new_links)
-    #         UserRepo.objects.bulk_create(new_links)
-
-
-# def hook(data):
-# 	header_signature = request.headers.get('X-Hub-Signature')
-# 	if header_signature is None:
-# 		abort(403)
-#
-# 	sha_name, signature = header_signature.split('=')
-# 	if sha_name != 'sha1':
-# 		abort(501)
-#
-# 	# HMAC requires the key to be bytes, but data is string
-# 	mac = hmac.new(str(secret), msg=request.data, digestmod='sha1')
-#
-# 	# Python prior to 2.7.7 does not have hmac.compare_digest
-# 	if hexversion >= 0x020707F0:
-# 		if not hmac.compare_digest(str(mac.hexdigest()), str(signature)):
-# 			abort(403)
-# 	else:
-# 		# What compare_digest provides is protection against timing
-# 		# attacks; we can live without this protection for a web-based
-# 		# application
-# 		if not str(mac.hexdigest()) == str(signature):
-# 			abort(403)

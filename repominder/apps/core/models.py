2bd836ade3fef38aa4df4dea11eeaca21e435355
Simon Weber <simon@simonmweber.com>
2021-03-27 21:22:32 -0400

    fix profile str

--- repominder/apps/core/models.py
+++ repominder/apps/core/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return "<Profile: self.user>" % self.user
+        return "<Profile: %s>" % self.user
 
     __repr__ = __str__
 

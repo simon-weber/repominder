19a3ab0846be329b739e784182cc31425ec3a3db
Simon Weber <simon@simonmweber.com>
2022-03-14 17:43:21 -0400

    django 3.2 + related upgrades

--- repominder/settings.py
+++ repominder/settings.py
@@ -90,6 +90,7 @@ WSGI_APPLICATION = "repominder.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",

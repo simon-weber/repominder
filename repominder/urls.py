ff86c9cfa73016d6d093aa55636bba7b5aa2d8ab
Simon Weber <simon@simonmweber.com>
2025-07-14 19:55:55 -0400

    fix urls

--- repominder/urls.py
+++ repominder/urls.py
@@ -1,24 +1,24 @@
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import include, re_path
 
 from .apps.core import views
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url("", include("social_django.urls", namespace="social")),
-    url(r"^account/$", views.account, name="account"),
-    url(r"^watch/(?P<id>\d+)/$", views.repo_details, name="repo"),
-    url(
+    re_path(r"^admin/", admin.site.urls),
+    re_path("", include("social_django.urls", namespace="social")),
+    re_path(r"^account/$", views.account, name="account"),
+    re_path(r"^watch/(?P<id>\d+)/$", views.repo_details, name="repo"),
+    re_path(
         r"^badge/(?P<selector>.+)/$",
         views.badge_info,
         name="badge_info",
     ),
-    url(
+    re_path(
         r"^hook/$",
         views.receive_hook,
         name="receive_hook",
     ),
-    url(r"^privacy/$", views.PrivacyView.as_view(), name="privacy"),
-    url(r"^logout/$", views.logout, name="logout"),
-    url(r"^$", views.LoggedOutView.as_view(), name="landing"),
+    re_path(r"^privacy/$", views.PrivacyView.as_view(), name="privacy"),
+    re_path(r"^logout/$", views.logout, name="logout"),
+    re_path(r"^$", views.LoggedOutView.as_view(), name="landing"),
 ]

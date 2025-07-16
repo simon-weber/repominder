19927ab1a38f8b593d81774c6f13fd5eb05da9ca
Simon Weber <simon@simonmweber.com>
2025-07-15 21:12:22 -0400

    switch backups to r2

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -12,7 +12,7 @@ s3_path="repominder/$(date +'%Y-%m-%d_%X')_${db_name}"
 backup_path="/tmp/${db_name}.bak"
 
 sqlite3 "${db_path}" ".backup '${backup_path}'"
-s3cmd --bucket-location=fr-par --host=https://s3.fr-par.scw.cloud --host-bucket=https://s3.fr-par.scw.cloud \
+s3cmd --bucket-location=auto --host=https://$CF_ACCOUNT_ID.r2.cloudflarestorage.com --host-bucket=https://$CF_ACCOUNT_ID.r2.cloudflarestorage.com \
   put "${backup_path}" "s3://simoncodes-sqlite/${s3_path}"
 
 wget https://hc-ping.com/${HC_ID_BACKUP} -nv -T 10 -t 5 -O /dev/null
